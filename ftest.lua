f0cc99fbc1315bc253d03246d27a5e9bd2b64ecd
rxcmr <45092328+rxcmr@users.noreply.github.com>
2019-05-10 10:00:55 +0800

    this is not the rotten apple

---
+++ ftest.lua
@@ -0,0 +1,68 @@
+-- PUT THIS IN /var/tweak/com.r333d.eqe/db/presets
+
+return {
+    {
+        name = "preamp",
+        gain = 0,
+    },
+    {
+        name = "eq",
+        frequency = 60,
+        Q = 1.4,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 1000,
+        Q = 1,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 3000,
+        Q = 1.4,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 100,
+        Q = 1,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 200,
+        Q = 1.4,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 225,
+        Q = 1.4,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 250,
+        Q = 1.4,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 5000,
+        Q = 2.8,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 7000,
+        Q = 2.8,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 10000,
+        Q = 1,
+        gain = 2.5,
+    },
+}
