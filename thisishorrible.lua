b8a90253706da5dfcf644ddfc010819a0ceeaa4f
rxcmr <45092328+rxcmr@users.noreply.github.com>
2019-05-10 10:02:26 +0800

    this is rotten as fuck

---
+++ thisishorrible.lua
@@ -0,0 +1,146 @@
+-- PUT THIS IN /var/tweak/com.r333d.eqe/db/presets
+
+return {
+    {
+        name = "preamp",
+        gain = -1.3279459476471,
+    },
+    {
+        name = "eq",
+        frequency = 60,
+        Q = 1.4,
+        gain = 0.432088,
+    },
+    {
+        name = "eq",
+        frequency = 1000,
+        Q = 1,
+        gain = -2.5,
+    },
+    {
+        name = "eq",
+        frequency = 3000,
+        Q = 1.4,
+        gain = -2.5,
+    },
+    {
+        name = "eq",
+        frequency = 100,
+        Q = 1,
+        gain = -2.5,
+    },
+    {
+        name = "eq",
+        frequency = 200,
+        Q = 1.4,
+        gain = 0.006348,
+    },
+    {
+        name = "eq",
+        frequency = 225,
+        Q = 1.4,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 250,
+        Q = 1.4,
+        gain = 0.753488,
+    },
+    {
+        name = "eq",
+        frequency = 5000,
+        Q = 1.28,
+        gain = -1,
+    },
+    {
+        name = "eq",
+        frequency = 7000,
+        Q = 2.8,
+        gain = 0.049179,
+    },
+    {
+        name = "eq",
+        frequency = 10000,
+        Q = 4.3,
+        gain = -0.435122,
+    },
+    {
+        name = "eq",
+        frequency = 2000,
+        Q = 1.6,
+        gain = 2.424876,
+    },
+    {
+        name = "eq",
+        frequency = 500,
+        Q = 0.197597,
+        gain = 2.5,
+    },
+    {
+        name = "eq",
+        frequency = 4000,
+        Q = 1.45,
+        gain = -0.6,
+    },
+    {
+        name = "eq",
+        frequency = 20,
+        Q = 6,
+        gain = 1,
+    },
+    {
+        name = "eq",
+        frequency = 40,
+        Q = 0.75,
+        gain = 2.358805,
+    },
+    {
+        name = "eq",
+        frequency = 300,
+        Q = 1,
+        gain = 0.680539,
+    },
+    {
+        name = "eq",
+        frequency = 350,
+        Q = 0.453424,
+        gain = 0.683761,
+    },
+    {
+        name = "eq",
+        frequency = 16000,
+        Q = 0.3,
+        gain = 1.176446,
+    },
+    {
+        name = "eq",
+        frequency = 20000,
+        Q = 0.110644,
+        gain = 2.2232,
+    },
+    {
+        name = "eq",
+        frequency = 8500,
+        Q = 5.588563,
+        gain = -0.387711,
+    },
+    {
+        name = "eq",
+        frequency = 9000,
+        Q = 1,
+        gain = 1.190138,
+    },
+    {
+        name = "eq",
+        frequency = 8000,
+        Q = 0.4,
+        gain = 0,
+    },
+    {
+        name = "eq",
+        frequency = 750,
+        Q = 0.05,
+        gain = 0.471696,
+    },
+}
